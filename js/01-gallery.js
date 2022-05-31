e469f2b002d27134c926f6bcd5c1152dc86cdff9
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-05-31 16:01:46 +0300

    Виконав завдання 1

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+const containerGalleryEl = document.querySelector('.gallery');
+
+const listItemsMarkup = createGalleryItems(galleryItems);
+containerGalleryEl.innerHTML = listItemsMarkup;
+
+function createGalleryItems(items) {
+    return items.map(({ preview, original, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`).join('');
+};
+
+
+function onImagesOpen(event) {
+
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG') return;
+    
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`)
+    
+    instance.show()
+    
+};
+
+containerGalleryEl.addEventListener('click', onImagesOpen);
+
+console.log(galleryItems);
